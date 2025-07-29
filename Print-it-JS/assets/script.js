1dcbbb61acfef262450653f16125631e7d40b0b7
Maxime <maxime.drouhin71@gmail.com>
2025-07-29 20:28:25 +0200

    factorisation du code en fonctions

--- Print-it-JS/assets/script.js
+++ Print-it-JS/assets/script.js
@@ -19,33 +19,74 @@ const slides = [
 
 const leftArrow = document.getElementById("leftArrow");
 const rightArrow = document.getElementById("rightArrow");
-const slideDots = document.querySelector(".dots");
-const bannerImg = document.querySelector(".banner-img");
-const bannerTxt = document.querySelector("#banner p");
 
 let slideNumber = 0;
 
-leftArrow.addEventListener("click", () => {
-	slideNumber--;
-	let newText = slides[slideNumber].tagLine;
-	bannerTxt.innerHTML = newText;
-	bannerImg.setAttribute("src", `./assets/images/slideshow/${slides[slideNumber].image}`);
+createBulletPoints();
+selectActiveDot(slideNumber);
 
+leftArrow.addEventListener("click", () => {
+	if (slideNumber === 0) {
+		slideNumber = slides.length - 1;
+	}
+	else {
+		slideNumber--;
+	}
+	moveSlider(slideNumber);
+	selectActiveDot(slideNumber);
 })
 
 rightArrow.addEventListener("click", () => {
-	slideNumber++;
-	let newText = slides[slideNumber].tagLine;
-	bannerTxt.innerHTML = newText;
-	bannerImg.setAttribute("src", `./assets/images/slideshow/${slides[slideNumber].image}`);
+	if (slideNumber === slides.length - 1) {
+		slideNumber = 0;
+	}
+	else {
+		slideNumber++;
+	}
+	moveSlider(slideNumber);
+	selectActiveDot(slideNumber);
 })
 
-//ajout dots
-for (let i = 1; i <= slides.length; i++) {
-	let dot = document.createElement("div");
-	dot.classList.add("dot");
-	dot.setAttribute("id", `dot${i}`);
-	slideDots.appendChild(dot);
+/**
+ * @param {Number} index : variable to iterate through "slides" table
+ */
+function moveSlider(index) {
+	const bannerImg = document.querySelector(".banner-img");
+	const bannerTxt = document.querySelector("#banner p");
+	let newText = slides[index].tagLine;
+	bannerTxt.innerHTML = newText;
+	bannerImg.setAttribute("src", `./assets/images/slideshow/${slides[index].image}`);
 }
+
+/**
+ * @param {Number} index : variable to iterate through "dot" list
+ */
+function selectActiveDot(index) {
+	let dotList = document.querySelectorAll(".dot");
+	let className = "dot_selected";
+	//remove the class on every element for each function call
+	for (let i = 0; i < dotList.length; i++) {
+		if (dotList[i].classList.contains(className)) {
+			dotList[i].classList.remove(className);
+		}
+	}
+	//adding the class on the targeted element
+	dotList[index].classList.add(className);
+}
+
+/**
+ * create bullet points to follow slide order
+ */
+function createBulletPoints() {
+	const slideDots = document.querySelector(".dots");
+
+	for (let i = 1; i <= slides.length; i++) {
+		let dotDiv = document.createElement("div");
+		dotDiv.classList.add("dot");
+		dotDiv.setAttribute("id", `dot${i}`);
+		slideDots.appendChild(dotDiv);
+	}
+}
+
 
 
